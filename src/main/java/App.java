99e456d2972a2ada191ba73b81b5b3fb9874a0f4
adrgimmun <adrgimmun@PRN5-10365702>
2026-02-27 16:44:39 +0100

    Creacion estructura del proyecto, modificacion de pom.xml, he añadido el archivo .jasper, he creado el jframe donde pulsas el boton y he hecho todo el codigo de FrmReportes con su App.java para ejecutar el proyecto

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+public class App {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new gui.FrmPrincipal().setVisible(true);
+        });
+    }
+}
